300cc42216a225a33915aecfef885ae2acbb13ee
Mefizz <58421356+Mefizz@users.noreply.github.com>
2026-07-10 13:31:12 +0300

    #267 Fix approvals filter, dynamic access levels, and signature validation

--- app/Livewire/Encounter/Forms/EncounterForm.php
+++ app/Livewire/Encounter/Forms/EncounterForm.php
@@ -1213,14 +1213,6 @@ class EncounterForm extends BaseForm
 
                 return;
             }
-
-            if (
-                !$hasActionReferences
-                && empty($this->diagnosticReports)
-                && empty($this->procedures)
-            ) {
-                $fail(__('validation.custom.encounter.actionReferences.required_activity'));
-            }
         };
     }
 
